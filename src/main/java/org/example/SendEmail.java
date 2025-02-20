5b28eac12e7464822902b2c1e69292bc989599ca
sozin <sozin.vladislav@mail.ru>
2025-02-20 14:23:06 +0300

    250220 commit

--- src/main/java/org/example/SendEmail.java
+++ src/main/java/org/example/SendEmail.java
@@ -7,12 +7,13 @@ import javax.mail.internet.*;
 public class SendEmail {
     public static void main(String[] args) {
 
+//    Здесь инфо, как создать рассылку: https://dev.to/suprsend/how-to-send-email-notifications-using-java-3-methods-with-code-examples-2ckd
 //    Как создать пароль для внешнего сервиса , для внешнего приложения:
 //   • Перейдите на страницу безопасности вашей учетной записи Mail.ru.
 //   • Найдите раздел, связанный с "Паролями приложений" или "Доступом приложений".
 //   • Создайте новый пароль приложения. Вам будет предоставлен уникальный код.
-//  Создаётся здесь: https://id.mail.ru/security
-//  В итоге создался здесь: https://account.mail.ru/user/2-step-auth/passwords?back_url=https%3A%2F%2Fid.mail.ru%2Fsecurity
+//    Создаётся здесь: https://id.mail.ru/security
+//    В итоге создался здесь: https://account.mail.ru/user/2-step-auth/passwords?back_url=https%3A%2F%2Fid.mail.ru%2Fsecurity
 
 
                 // Настройки SMTP-сервера Mail.ru
