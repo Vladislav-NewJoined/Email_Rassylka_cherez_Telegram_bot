6939a5c1c91dd3ce14d3691ef61927d8d6fc4df4
sozin <sozin.vladislav@mail.ru>
2025-02-20 14:06:53 +0300

    250220 Initial commit

---
+++ src/main/java/org/example/SendEmail.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class SendEmail {
+    public static void main(String[] args) {
+        // Настройки SMTP-сервера Mail.ru
+        String host = "smtp.mail.ru";
+        String port = "465"; // Порт для SSL
+        String username = "sozin.vladislav@mail.ru"; // Ваш email
+        String password = "XE2u0sb8qFyJttebpXuK"; // Ваш пароль
+//        String password = "sozsoz1967.A"; // Ваш пароль
+
+        // Настройка свойств
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true"); // Включаем аутентификацию
+        props.put("mail.smtp.ssl.enable", "true"); // Включаем SSL для порта 465
+        props.put("mail.smtp.host", host); // Указываем SMTP-сервер
+        props.put("mail.smtp.port", port); // Указываем порт для SSL
+
+        // Создание сессии
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        // Включаем режим отладки
+        session.setDebug(true);
+
+        try {
+            // Создание сообщения
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("sozin.vladislav@mail.ru"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("sozin.vladislav@mail.ru"));
+            message.setSubject("Тема письма");
+            message.setText("Текст письма");
+
+            // Отправка сообщения
+            Transport.send(message);
+
+            System.out.println("Письмо успешно отправлено.");
+
+        } catch (MessagingException e) {
+            e.printStackTrace(); // Выводим полный стек ошибки
+            throw new RuntimeException(e);
+        }
+    }
+}
